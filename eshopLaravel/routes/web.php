af0edd266da404fdb047689a4f2b82a03c05abf7
xnovakb <noboris.gymnazium@gmail.com>
2023-04-16 14:47:12 +0200

    2. Fáza fix routes

--- eshopLaravel/routes/web.php
+++ eshopLaravel/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\UserController;
-=======
-use App\Http\Controllers\CartController;
->>>>>>> ee8231b1aa7d1fc33af338584c4340c1c6e9e55f
 
 /*
 |--------------------------------------------------------------------------
@@ -33,18 +29,8 @@ Route::get('/products/{id}', function () {
     return view('detailOfProduct');
 });
 
-<<<<<<< HEAD
 // Registration of user
 Route::get('/register', [UserController::class, 'create']);
 
 // Create user
 Route::post('/user', [UserController::class, 'store']);
-=======
-//Cart prefix
-Route::prefix('cart')->group(function () {
-    Route::get('/summary', [CartController::class, 'summary']);
-    Route::get('/shipping', [CartController::class, 'shipping']);
-    Route::get('/payment', [CartController::class, 'payment']);
-    Route::get('/info', [CartController::class, 'info']);
-});
->>>>>>> ee8231b1aa7d1fc33af338584c4340c1c6e9e55f
