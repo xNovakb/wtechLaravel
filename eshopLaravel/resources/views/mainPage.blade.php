ca92295880cfee6b8471154d15deb42ee2e2e18c
tessyelots <marek.smutny166@gmail.com>
2023-04-28 16:37:57 +0200

    polish

--- eshopLaravel/resources/views/mainPage.blade.php
+++ eshopLaravel/resources/views/mainPage.blade.php
@@ -28,7 +28,7 @@
                     </div>
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link d-none d-md-inline " href="/cart/summary">
+                            <a class="nav-link d-none d-md-inline " href="/cart/summary/{{0}}">
                                 <i class="zmdi zmdi-shopping-cart-plus fs-2"></i>
                             </a>
                             <a class="nav-link d-md-none" href="#">Košík</a>
