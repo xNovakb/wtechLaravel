730fe43effa621b3098f8f6ebeeb2221b964983b
xnovakb <noboris.gymnazium@gmail.com>
2023-05-06 15:00:21 +0200

    2.Fáza: Added functionality for picture on detail and mainpage

--- eshopLaravel/resources/views/detailOfProduct.blade.php
+++ eshopLaravel/resources/views/detailOfProduct.blade.php
@@ -12,91 +12,95 @@
 </head>
 <body>
     <div class="container">
-        <nav class="navbar navbar-expand-md bg-body-tertiary py-3">
-            <div class="container-fluid justify-content-center px-4">
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="d-flex align-items-center flex-grow-1 justify-content-end">
-                  <form class="d-flex col-10" action="/" method="GET">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search">
-                    <button class="btn" type="submit">
-                      <i class="zmdi zmdi-search fs-2"></i>
-                    </button>
-                  </form>
-                </div>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                      <a class="nav-link d-none d-md-inline " href="/cart/summary">
-                          <i class="zmdi zmdi-shopping-cart-plus fs-2"></i>
-                      </a>
-                      <a class="nav-link d-md-none" href="#">Košík</a>
-                  </li>
-                  <li class="nav-item">
-                      @if (Auth::check())
-                      <form action="/users/logout" method="POST">
-                          @csrf
-                          <button type="submit" class="nav-link d-none d-md-inline btn btn-link nav-link-button">
-                              <i class="zmdi zmdi-lock-outline fs-2"></i>
-                          </button>
-                          <button type="submit" class="nav-link d-md-none btn btn-link nav-link-button">Odhlásiť sa</button>
+        <script src="{{ asset('js/mainPage.js') }}"></script>
+        <header>
+            <nav class="navbar navbar-expand-md bg-body-tertiary py-3">
+                <div class="container-fluid justify-content-center px-4">
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="d-flex align-items-center flex-grow-1 justify-content-end">
+                      <form class="d-flex col-10" action="/" method="GET">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search">
+                        <button class="btn" type="submit">
+                          <i class="zmdi zmdi-search fs-2"></i>
+                        </button>
                       </form>
-                      @else
-                          <a class="nav-link d-none d-md-inline" href="/login">
-                              <i class="zmdi zmdi-lock-open fs-2"></i>
-                          </a>
-                      @endif
-                  </li>
-              </ul>
-              </div>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="zmdi zmdi-menu fs-2"></i>
-              </button>
-          </nav>
+                    </div>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link d-none d-md-inline " href="/cart/summary">
+                                <i class="zmdi zmdi-shopping-cart-plus fs-2"></i>
+                            </a>
+                            <a class="nav-link d-md-none btn btn-link nav-link-button" href="/cart/summary">Košík</a>
+                        </li>
+                        <li class="nav-item">
+                            @if (Auth::check())
+                            <form action="/users/logout" method="POST">
+                                @csrf
+                                <button type="submit" class="nav-link d-none d-md-inline btn btn-link nav-link-button">
+                                    <i class="zmdi zmdi-lock-outline fs-2"></i>
+                                </button>
+                                <button type="submit" class="nav-link d-md-none btn btn-link nav-link-button">Odhlásiť sa</button>
+                            </form>
+                            @else
+                                <a class="nav-link d-none d-md-inline" href="/login">
+                                    <i class="zmdi zmdi-lock-open fs-2"></i>
+                                </a>
+                                <a href="/login" class="nav-link d-md-none btn btn-link nav-link-button">Prihlasiť sa</a>
+                            @endif
+                        </li>
+                    </ul>
+                  </div>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="zmdi zmdi-menu fs-2"></i>
+                  </button>
+              </nav>
+        </header>
         <div class="row">
             <div class="col-md-4 d-none d-md-block">
                 <div class="row">
-                <div class="col-md-12">
-                    <img class="img-fluid big-img" src="https://via.placeholder.com/600x600" alt="Product Image">
-                </div>
+                    <div class="col-md-12">
+                        @if(count($product->images) > 0)
+                            <img class="img-fluid big-img" src="{{ asset('storage/' . $product->images[0]->image) }}" alt="Product Image">
+                        @else
+                            <img class="img-fluid big-img" src="https://via.placeholder.com/600x600" alt="Product Image">
+                        @endif
+                    </div>
                 </div>
                 <div class="row mt-3">
-                <div class="col-md-4 mb-2">
-                    <img class="img-fluid small-img" src="https://via.placeholder.com/150x150" alt="Product Image">
-                </div>
-                <div class="col-md-4 mb-2">
-                    <img class="img-fluid small-img" src="https://via.placeholder.com/150x150" alt="Product Image">
-                </div>
-                <div class="col-md-4 mb-2">
-                    <img class="img-fluid small-img" src="https://via.placeholder.com/150x150" alt="Product Image">
-                </div>
+                    @foreach ($product->images as $key => $image)
+                        @if ($key > 0)
+                            <div class="col-md-4 mb-2">
+                                <img class="img-fluid small-img" src="{{ asset('storage/' . $image->image) }}" alt="Product Image">
+                            </div>
+                        @endif
+                    @endforeach
                 </div>
             </div>
             
             <div class="col-xs-12 d-md-none">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li> 
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img class="d-block w-100" src="https://via.placeholder.com/600x600" alt="First slide">
+                <div class="card text-center">
+                    <div id="carousel-{{ $product->id }}" class="carousel slide" data-bs-interval="false">
+                        <div class="carousel-indicators">
+                            @foreach ($product->images as $key => $image)
+                                <button type="button" data-bs-target="#carousel-{{ $product->id }}" data-bs-slide-to="{{ $key }}" class="{{ $key === 0 ? 'active' : '' }}"></button>
+                            @endforeach
+                        </div>
+                        <div class="carousel-inner">
+                            @foreach ($product->images as $key => $image)
+                                <div class="carousel-item {{ $key === 0 ? 'active' : '' }} cItem">
+                                    <img src="{{ asset('storage/' . $image->image) }}" class="d-block w-100 cImg" alt="Product Image">
+                                </div>
+                            @endforeach
+                        </div>
+                        <a class="carousel-control-prev" href="#carousel-{{ $product->id }}" role="button" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carousel-{{ $product->id }}" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </a>
                     </div>
-                    <div class="carousel-item">
-                    <img class="d-block w-100" src="https://via.placeholder.com/600x600" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="d-block w-100" src="https://via.placeholder.com/600x600" alt="Third slide">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
                 </div>
             </div>
 
@@ -121,5 +125,7 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 </body>
 </html>
--- eshopLaravel/resources/views/mainPage.blade.php
+++ eshopLaravel/resources/views/mainPage.blade.php
@@ -31,7 +31,7 @@
                             <a class="nav-link d-none d-md-inline " href="/cart/summary">
                                 <i class="zmdi zmdi-shopping-cart-plus fs-2"></i>
                             </a>
-                            <a class="nav-link d-md-none" href="#">Košík</a>
+                            <a class="nav-link d-md-none btn btn-link nav-link-button" href="/cart/summary">Košík</a>
                         </li>
                         <li class="nav-item">
                             @if (Auth::check())
@@ -46,6 +46,7 @@
                                 <a class="nav-link d-none d-md-inline" href="/login">
                                     <i class="zmdi zmdi-lock-open fs-2"></i>
                                 </a>
+                                <a href="/login" class="nav-link d-md-none btn btn-link nav-link-button">Prihlasiť sa</a>
                             @endif
                         </li>
                     </ul>
@@ -118,31 +119,28 @@
                         @foreach ($products as $product)
                         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3 py-2">
                             <div class="card text-center">
-                                <div id="" class="carousel slide">
+                                <a href="/product/{{$product->id}}" class="text-decoration-none text-dark">
+                                <div id="carousel-{{ $product->id }}" class="carousel slide" data-bs-interval="false">
                                     <div class="carousel-indicators">
-                                        <button type="button" data-bs-target="" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                        <button type="button" data-bs-target="" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                        <button type="button" data-bs-target="" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                        @foreach ($product->images as $key => $image)
+                                            <button type="button" data-bs-target="#carousel-{{ $product->id }}" data-bs-slide-to="{{ $key }}" class="{{ $key === 0 ? 'active' : '' }}"></button>
+                                        @endforeach
                                     </div>
                                     <div class="carousel-inner">
-                                        <div class="carousel-item active cItem">
-                                            <img src="assets/boots.png" class="d-block w-100 cImg" alt="...">
-                                        </div>
-                                        <div class="carousel-item cItem">
-                                            <img src="assets/dress.jpg" class="d-block w-100 cImg" alt="...">
-                                        </div>
-                                        <div class="carousel-item cItem">
-                                            <img src="assets/boots.png" class="d-block w-100 cImg" alt="...">
-                                       </div>
+                                        @foreach ($product->images as $key => $image)
+                                            <div class="carousel-item {{ $key === 0 ? 'active' : '' }} cItem">
+                                                <img src="{{ asset('storage/' . $image->image) }}" class="d-block w-100 cImg" alt="Product Image">
+                                            </div>
+                                        @endforeach
                                     </div>
-                                    <button class="carousel-control-prev" type="button" data-bs-target="#hero-carousel" data-bs-slide="prev">
+                                    <a class="carousel-control-prev" href="#carousel-{{ $product->id }}" role="button" data-bs-slide="prev">
                                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                         <span class="visually-hidden">Previous</span>
-                                    </button>
-                                    <button class="carousel-control-next" type="button" data-bs-target="#hero-carousel" data-bs-slide="next">
+                                    </a>
+                                    <a class="carousel-control-next" href="#carousel-{{ $product->id }}" role="button" data-bs-slide="next">
                                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                         <span class="visually-hidden">Next</span>
-                                    </button>
+                                    </a>
                                 </div>
                                 <div class="card-body justify-content-center">
                                     <h5 class="card-title col-12">{{ $product['name'] }}</h5>
@@ -159,8 +157,9 @@
                                         <li class="list-inline-item">{{ $product['price'] }}€</li>
                                     </ul>
                                 </div>
+                            </a>
                             </div>
-                        </div>    
+                        </div>
                         @endforeach
                     </div>
                 </div>
@@ -172,6 +171,7 @@
                 {{ $products->appends(['search' => request('search'), 'sort' => request('sort'), 'price_from' => request('price_from'), 'price_to' => request('price_to'), 'color' => request('color'), 'brand' => request('brand')])->links() }}
             </div>
         </footer>
-        
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
     </body>
 </html>
